716f2361b27b5a6add77a45f1d7ab4f0eeec94d7
nickbartley <nicholas.bartley@sca.com.au>
2024-06-26 22:32:51 +1000

    Delete Function Added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController <  ApplicationController
         @article = Article.new
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
         if @article.save
@@ -20,4 +24,24 @@ class ArticlesController <  ApplicationController
             render 'new'
         end
     end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:Notice] = "Article was updated succesfully"
+            redirect_to @article 
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:id]) 
+        @article.destroy                 
+        redirect_to articles_path    
+    end
+
+
 end
+
+   
