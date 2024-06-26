f81cd7dedb828a828079ecb24e8b44eca520f8e7
nickbartley <nicholas.bartley@sca.com.au>
2024-06-26 13:28:06 +1000

    Created an articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :articles, only: [:show]
 end
